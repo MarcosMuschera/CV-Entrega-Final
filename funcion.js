487d3c7ede2879bfbb3ff140199566fb09e2d5dc
Marcos Muschera <Marcos.muschera@gmail.com>
2023-02-27 18:37:01 -0300

    archivos

---
+++ funcion.js
@@ -0,0 +1,11 @@
+document.getElementById('boton1').addEventListener('click', function() (
+    document.getElementById('img2').style.display = '';
+));
+
+document.getElementById('boton2').addEventListener('click', function() (
+    document.getElementById('img3').style.display = '';
+));
+
+document.getElementById('boton3').addEventListener('click', function() (
+    document.getElementById('columna left').style.display = '';
+));
